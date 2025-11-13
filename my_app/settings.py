4422c224493d0b8fb9e44bc49084751d3880912b
Prantik Pal <developerpsquare356@gmail.com>
2025-11-13 23:41:27 +0530

    Update settings.py

--- my_app/settings.py
+++ my_app/settings.py
@@ -138,7 +138,8 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIR={os.path.join(BASE_DIR,"demo/static")}
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'public/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL='/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -153,6 +154,7 @@ EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER="developerpsquare356@gmail.com"
 EMAIL_HOST_PASSWORD="tnkowxxvbalzxdql"
+
 
 
 
